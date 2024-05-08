21203c56f68b1620e2a1e8df1946b4ba31d201a9
Brendy <brandyaguilar001@gmail.com>
2024-05-08 13:31:39 -0600

    ejemplo de una función derivada

--- inetgrales.m
+++ inetgrales.m
@@ -1,6 +1,25 @@
-*ITESS-TICS
-*Enero-Junio
-
+%% ITESS-TICS
+%% Enero-Junio
+%% Brandy Aguilar Flores
+%% Calculo Integral
+%% Derivadas e integrales simbolicas
 
 pkg load symbolic
 sysms x
+
+%% Definir una función
+disp('una funcion')
+fx = x * sin (x)
+
+%% derivar
+disp('derivada')
+dfx = diff(fx, x)
+
+%% integral indefinida
+disp('Integral indefinida')
+ifx = int(fx,x)
+
+%% integral definida
+disp('integral definida')
+ifx = double ( int (fx,x, 0,1) )
+
